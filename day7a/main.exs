51628ce3a4a3f9d258024ab41c0e88a96910fea2
Vivien AUGUY <vivien.auguy@talan.com>
2021-12-22 17:42:37 +0100

    day 7a

---
+++ day7a/main.exs
@@ -0,0 +1,45 @@
+require Integer
+
+defmodule Main do
+
+  def run(file) do
+
+    positions = File.read!(Path.join(__DIR__, file))
+                |> String.split([",", "\n"], trim: true)
+                |> Enum.map(&String.to_integer/1)
+                |> Enum.sort
+                |> IO.inspect
+
+    median = positions
+             |> compute_median
+             |> IO.inspect
+
+    positions
+    |> Enum.reduce(0, fn position, acc -> acc + abs(position - median) end)
+  end
+  # exemple 16,1,2,0,4,2,7,1,2,14 => 2 best place => 37 fuel
+
+  def compute_median(positions) do
+    size = Enum.count(positions)
+    if(Integer.is_odd(size)) do
+      # impair
+      Enum.at(positions, round(size / 2))
+    else
+      # pair
+      medianIndex = trunc(size / 2) - 1
+      positions
+      |> Enum.slice(medianIndex, 2)
+      |> Enum.sum
+      |> Kernel./(2)
+      |> Kernel.round
+    end
+  end
+
+end
+
+IO.puts "*********** Running Exercice ***********"
+Main.run("input.txt")
+|> IO.inspect
+IO.puts "\n*********** Running Exemple ***********"
+Main.run("exemple.txt")
+|> IO.inspect
